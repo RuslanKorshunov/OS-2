83c660e9e949c3b38e49417e5d56ad631e6a5ea6
Руслан <ruslan.korsh@yandex.ru>
2018-12-15 01:36:19 +0300

    Added completelly logging

--- ConsoleProcess/ConsoleProcess.cpp
+++ ConsoleProcess/ConsoleProcess.cpp
@@ -5,36 +5,39 @@
 #include "../OS-2/WriteInFile.h"
 using namespace std;
 
-void main(VOID)
+#define MAP_VIEW_OF_FILE_ERROR 2
+#define OPEN_SEMAPHORE_ERROR 3
+#define OPEN_FILE_MAPPING_ERROR 4
+
+int main()
 {
 	WriteInFile writeInFile;
 	HANDLE semaphore = OpenSemaphore(SEMAPHORE_ALL_ACCESS, FALSE, TEXT("MainProcessSemaphore"));
 	HANDLE fileMapping = OpenFileMapping(FILE_MAP_READ, FALSE, TEXT("FileMapping"));
 
 
-	bool key = true;
+	//bool key = true;
 	if (semaphore == NULL)
 	{
-		cout << "Error of OpenSemaphore" << endl;
-		key = false;
+		writeInFile.writeErrorInLog("Error of OpenSemaphore", OPEN_SEMAPHORE_ERROR);
+		return OPEN_SEMAPHORE_ERROR;
 	}
 	if (fileMapping == NULL)
 	{
-		cout << "Error of OpenFileMapping" << endl;
-		key = false;
+		writeInFile.writeErrorInLog("Error of OpenFileMapping", OPEN_FILE_MAPPING_ERROR);
+		return OPEN_FILE_MAPPING_ERROR;
 	}
 	char* ch = (char*)MapViewOfFile(fileMapping, FILE_MAP_READ, 0, 0, 0);
 	if (ch == NULL)
 	{
-		cout << "Error of MapViewOfFile" << endl;
-		key = false;
+		writeInFile.writeErrorInLog("Error of MapViewOfFile", MAP_VIEW_OF_FILE_ERROR);
+		return MAP_VIEW_OF_FILE_ERROR;
 	}
-	if (key != false)
-		while (true)
-		{
-			WaitForSingleObject(semaphore, INFINITE);
-			cout << "CP: ";
-			cout << ch << "\n";
-			writeInFile.writeInLog("CP", ch);
-		}
+	while (true)
+	{
+		WaitForSingleObject(semaphore, INFINITE);
+		cout << "CP: ";
+		cout << ch << "\n";
+		writeInFile.writeInLog("CP", ch);
+	}
 }
--- OS-2/WriteInFile.cpp
+++ OS-2/WriteInFile.cpp
@@ -2,15 +2,17 @@
 #include <stdio.h>
 #include <string>
 #include <iostream>
-//#include <fstream>
 #include "WriteInFile.h"
 using namespace std;
 
 
 void WriteInFile::clearFile()
 {
-	fstream fout(FILE_NAME);
-	fout.open(FILE_LOG_NAME);
+	fstream fout(FILE_NAME, fstream::trunc|fstream::app);
+	fout << "";
+	fout.close();
+	fout.open(FILE_LOG_NAME, fstream::trunc|fstream::app);
+	fout << "";
 	fout.close();
 }
 void WriteInFile::writeNumberInFile(char*number)
@@ -27,12 +29,11 @@ void WriteInFile::writeInLog(char* process, char* number)
 {
 	fstream fout(FILE_LOG_NAME, ios_base::app);
 
-	/*SYSTEMTIME systemtime;
+	SYSTEMTIME systemtime;
 	GetLocalTime(&systemtime);
 	string s_time = to_string(systemtime.wHour) + ':' + to_string(systemtime.wMinute) + ':' + to_string(systemtime.wSecond) + ':' + to_string(systemtime.wMilliseconds);
-	const char* ch_time = s_time.c_str();*/
-
-	fout << getTime() << "  " << process << "  " << number << endl;
+	const char* ch_time = s_time.c_str();
+	fout << ch_time << "  " << process << "  " << number << endl;
 
 	fout.close();
 }
@@ -49,7 +50,11 @@ void WriteInFile::writeErrorInLog(char* message, int codeOfError)
 {
 	fstream fout(FILE_LOG_NAME, ios_base::app);
 
-	fout << getTime() << "  " << message << "  " << codeOfError << endl;
+	SYSTEMTIME systemtime;
+	GetLocalTime(&systemtime);
+	string s_time = to_string(systemtime.wHour) + ':' + to_string(systemtime.wMinute) + ':' + to_string(systemtime.wSecond) + ':' + to_string(systemtime.wMilliseconds);
+	const char* ch_time = s_time.c_str();
+	fout << ch_time << "  " << message << "  " << codeOfError << endl;
 
 	fout.close();
 }
